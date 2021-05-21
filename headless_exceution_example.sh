7739c54c7c092da190e7ca79e66c015a1535aa19
Julian McGinnis <julian.mcginnis@tum.de>
2021-05-21 19:27:25 +0200

    add 2nd workspace for headless execution  of veselgraphextraction

---
+++ headless_exceution_example.sh
@@ -0,0 +1,1 @@
+/voreentool --workspace /home/juli/tum/git_repositories/voreen/feature-vesselgraphextraction_customized_command_line.vws -platform minimal --trigger-volumesaves --trigger-geometrysaves
