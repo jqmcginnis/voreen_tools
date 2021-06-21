8e873409d26294fc86867f811547febd2b7375b9
Julian McGinnis <33037028+jqmcginnis@users.noreply.github.com>
2021-06-21 14:23:32 +0200

    Delete headless_exceution_example.sh

--- headless_exceution_example.sh
+++
@@ -1,1 +0,0 @@
-/voreentool --workspace /home/juli/tum/git_repositories/voreen/feature-vesselgraphextraction_customized_command_line.vws -platform minimal --trigger-volumesaves --trigger-geometrysaves
